6b3205134666f35305018761a07762f445a17b48
Faysal Jamil Nadim <faysalnadim6@gmail.com>
2023-07-17 22:37:38 +0600

    Bug Fix

--- src/Routes/cart.js
+++ src/Routes/cart.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { addToCart, getCart } = require("../Controllers/cart");
+const { addToCart, getCart, removeCart } = require("../Controllers/cart");
 const { requireSignIn, userMiddleware } = require("../Middlewares");
 const router = express.Router();
 
 router.post("/user/cart/add", requireSignIn, userMiddleware, addToCart);
 router.get("/user/cart/get", requireSignIn, userMiddleware, getCart);
+router.post("/user/cart/remove", requireSignIn, userMiddleware, removeCart);
 
 module.exports = router;
